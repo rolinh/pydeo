f96fa8e706207733425a132190953dfcfeecc083
Robin Hahling <robin.hahling@gw-computing.net>
2013-07-13 11:36:57 +0200

    updated files_dir unit test

--- test/unit/files_dir_helper_test.py
+++ test/unit/files_dir_helper_test.py
@@ -8,6 +8,6 @@ class FilesDirHelperTests(unittest.TestCase):
 
     def test_check_dir_presence(self):
         assert files_dir.check_dir_presence(['/tmp', '/usr', '/usr/bin']) == 'OK'
-        assert files_dir.check_dir_presence(['/foo', '/bar']) != 'OK'
-        assert files_dir.check_dir_presence(['/usr/bin', '/bar']) != 'OK'
+        assert files_dir.check_dir_presence(['/foo', '/bar']) == '/foo'
+        assert files_dir.check_dir_presence(['/usr/bin', '/bar']) == '/bar'
 
