67098fbb03902065792088bd8514f9230da79194
gilliek <kevin.gillieron@gw-computing.net>
2013-07-29 21:25:51 +0200

    added a class for handling database connection

---
+++ lib/controllers/db_connector.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DbConnector:
+    session = None
+    engine = None
+
+    @staticmethod
+    def init(db_url='sqlite:///:memory:'):
+        DbConnector.engine = create_engine(db_url, echo=True)
+        Session = sessionmaker(bind=DbConnector.engine)
+        DbConnector.session = Session()
